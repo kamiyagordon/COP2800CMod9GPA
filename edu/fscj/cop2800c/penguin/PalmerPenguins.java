0f686651efcdd933bd6800b4f847db2593c08b94
Gordon <S3501678@students.fscj.edu>
2026-03-16 19:37:22 +0900

    Completed Module 9 assignment with output and screenshots

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,6 +1,6 @@
 // PalmerPenguins.java
-// D. Singletary
-// 2/22/25
+// Sakura Gordon
+// 3/16/2026
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 package edu.fscj.cop2800c.penguin;
@@ -8,7 +8,10 @@ package edu.fscj.cop2800c.penguin;
 public class PalmerPenguins {
     public static void main(String[] args) {
         // Create an instance of PenguinAnalyzer
+PenguinAnalyzer analyzer = new PenguinAnalyzer();
 
-
+analyzer.readPenguinData();
+analyzer.printPenguinData();
+analyzer.writePenguinData();
     }
 }
